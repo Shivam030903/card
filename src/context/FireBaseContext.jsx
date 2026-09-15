cc37f88fff2a1ba434a2df9486a3d1d2f1e0a9d4
Shivam030903 <shivamjain030903@gmail.com>
2026-09-15 22:50:51 +0530

    Fix main.jsx

--- src/context/FireBaseContext.jsx
+++ src/context/FireBaseContext.jsx
@@ -10,14 +10,13 @@ import {
 export const FireBaseDataContext = createContext(null);
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-mPSy93BBM3Yzwn5_BK38b_-ntDF1XL8",
-  authDomain: "study-card-8ba01.firebaseapp.com",
-  projectId: "study-card-8ba01",
-  storageBucket: "study-card-8ba01.firebasestorage.app",
-  messagingSenderId: "159701827709",
-  appId: "1:159701827709:web:48a4eb687b6e530c167114",
-  measurementId: "G-9C3TWFZ6PV",
-  databaseURL: "https://study-card-8ba01-default-rtdb.firebaseio.com",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
